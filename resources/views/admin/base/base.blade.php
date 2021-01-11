313a6970e1303851cc3b0bf0f3be83b186b2bb4d
Ahmad Naufal Hilmy <hilmyahmadnaufal@gmail.com>
2021-01-11 22:36:03 +0700

    feat: quality of life

--- resources/views/admin/base/base.blade.php
+++ resources/views/admin/base/base.blade.php
@@ -8,7 +8,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Starter</title>
+    @yield('meta')
+    <title>@yield('title', "AdminLTE 3 | Starter")</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
